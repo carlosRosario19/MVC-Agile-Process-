de78059313643171345595e4ec56122c592c9f32
Carlos Rosario <crosari5@my.centennialcollege.ca>
2025-03-14 20:26:37 -0400

    Adding a view

--- obj/Debug/net8.0/MvcMovieCarlos.AssemblyInfo.cs
+++ obj/Debug/net8.0/MvcMovieCarlos.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovieCarlos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b6053d4e889f78055c43d41da3d5fe38114ca668")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovieCarlos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovieCarlos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
