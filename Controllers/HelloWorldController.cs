9522a0f198abaa93ddd3b98335e263481b48c898
Carlos Rosario <crosari5@my.centennialcollege.ca>
2025-03-14 20:00:50 -0400

    Adding controller

--- Controllers/HelloWorldController.cs
+++ Controllers/HelloWorldController.cs
@@ -7,14 +7,12 @@ namespace MvcMovieCarlos.Controllers
     {
         // 
         // GET: /HelloWorld/
-
         public string Index()
         {
             return "This is my default action...";
         }
 
         // GET: /HelloWorld/Welcome/ 
-        // Requires using System.Text.Encodings.Web;
         public string Welcome(string name, int ID = 1)
         {
             return HtmlEncoder.Default.Encode($"Hello {name}, ID: {ID}");
